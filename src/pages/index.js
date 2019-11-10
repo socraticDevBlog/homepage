c593e3852d97d34a61e90c1eb5d2281b658e9d10
socraticDevBlog <maximebonin@outlook.com>
2019-11-10 17:18:22 -0500

    footer not ooter

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 import Header from '../components/header'
 import Main from '../components/main'
-import Footer from '../components/ooter'
+import Footer from '../components/footer'
 
 class IndexPage extends React.Component {
   constructor(props) {
