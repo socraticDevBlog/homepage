087db8a2e638a725b2fc519ac3e6d8d27b9488fa
socraticDevBlog <maximebonin@outlook.com>
2020-01-15 19:05:17 -0500

    linky to english site and minor correction in texts

--- src/components/main.js
+++ src/components/main.js
@@ -42,30 +42,26 @@ class Main extends React.Component {
           </a>
           <p>
             Passionné par les lectures denses et coriaces, j'ai rapidement
-            découvert les joies de la pensée abstraite. Lors du baccalauréat en
-            Philosophie, à l'Université Laval de Québec, j'ai été séduit par la
-            logique, l'épistémologie et la philosophie analytique du langage.
-            D'un tempérament artistique, je me suis fais un devoir de prendre
-            des bouchées doubles pour acquérir un minimum de rationalité et de
-            culture scientifique.
+            découvert les joies de la pensée abstraite. À l'université, j'ai été
+            séduit par la logique, l'épistémologie et la philosophie analytique
+            du langage. Je me suis fais un devoir de développer une pensée
+            critique et une culture scientifique.
           </p>
           <p>
-            Encore insatisfait de ma culture philosophique et désirant
-            poursuivre une carrière d'enseignant, j'ai complété une maîtrise en
-            Philosophie à l'Université du Québec à Montréal. J'ai écrit un long
-            mémoire au sujet du concept de sophisme. Cette forme d'argument
-            trompeur fascine la plupart des étudiants. Toutefois, le concept de
-            sophisme est équivoque. Il est mal défini et mal compris. Je crois
-            avoir été en mesure de pointer les lacunes de la conception naïve
-            des sophismes. Grâce à la théorie pragma-dialectique de
-            l'argumentation, nous sommes en mesure de mieux comprendre le sens
-            et le rôle de l'argumentation pour des humains raisonnables.
+            Visant une carrière dans l'enseignement, j'ai complété une maîtrise
+            en Philosophie à Montréal. J'ai écrit un long mémoire au sujet du
+            concept de sophisme. Cette forme d'argument trompeur fascine la
+            plupart des étudiants. Toutefois, le concept de sophisme est
+            équivoque. Il est mal défini et mal compris. J'ai été en mesure de
+            pointer les lacunes de la conception ordinaire des sophismes. Grâce
+            à la théorie pragma-dialectique de l'argumentation, nous sommes en
+            mesure de mieux comprendre le sens et le rôle de l'argumentation
+            pour des humains raisonnables.
           </p>
           <p>
             Aujourd'hui, entre un exercice de pensée abstraite et un art de
-            vivre, la philosophie est redevenue un passe-temps pour moi. Je suis
-            redevenu un avide lecteur de Platon et des philosophes de
-            l'Antiquité.
+            vivre, la philosophie est redevenue un passe-temps. Je suis redevenu
+            un avide lecteur de Platon et des philosophes de l'Antiquité.
           </p>
           {close}
         </article>
@@ -92,24 +88,21 @@ class Main extends React.Component {
           <p>
             Après une courte carrière dans l'enseignement, j'ai relevé le défi
             d'une réorientation professionnelle. Je voulais avant tout faire
-            partie de la révolution technologique du XXIe siècle et mettre à
-            profit mes compétences logiques dans la réalisation de projets
-            concrets.
+            partie de la révolution numérique du XXIe siècle et mettre à profit
+            mes compétences logiques et conceptuelles dans la réalisation de
+            projets concrets.
           </p>
           <p>
-            En sortant du collège, j'ai oeuvré au sein d'une équipe développant des
-            logiciels de gestion. J'ai d'abord assuré la maintenance du logiciel de gestion de cabinet médical. Ensuite, le développement
-            d'une application mobile destinée aux professionnels travaillant sur
-            la route a occupé tout mon temps.
+            En peu de temps, j'ai acquis de l'expérience sur de nombreux
+            systèmes: logiciels de gestion, services web, applications web,
+            traitements en lot. Au niveau des bases de données relationnelles :
+            scripts SQL et procédures stockées.
           </p>
           <p>
-            Je travaille présentement à titre de consultant. Je termine actuellement un mandat avec une
-            grande agence gouvernementale. J'appuie les efforts de l'équipe en
-            place pour maintenir et développer des solutions logicielles
-            robustes. Ce rôle me permet de mettre à profit mon leadership ainsi
-            que mes compétences logiques et techniques autant en développant de
-            nouvelles plate-formes Web qu'en rédigeant des scripts SQL et en
-            automatisant des traitements en lot.
+            J'oeuvre comme développeur dans l'écosystème Microsoft. Je
+            suis aussi présentement en formation afin d'acquérir les compétences
+            de développeur Big Data dans l'écosystème Hadoop. Et quand il me
+            reste du temps, j'aime coder en Python, JavaScript et Scheme.
           </p>
           {close}
         </article>
@@ -144,10 +137,9 @@ class Main extends React.Component {
             interventions.
           </p>
           <p>
-            Pour vraiment apprendre. Pour maîtriser un sujet, rien
-            ne vaut l'expérience de tenter de l'expliquer ; de l'enseigner. Au
-            final, ce blogue, je le maintiens d'abord pour mon profit personnel
-            !
+            Pour vraiment apprendre. Pour maîtriser un sujet, rien ne vaut
+            l'expérience de tenter de l'expliquer ; de l'enseigner. Au final, ce
+            blogue, je le maintiens d'abord pour mon profit personnel !
           </p>
           <p>
             <a
